84165e0b065a93c950102b4475826724e6954a34
fsddvfg221 <fsddvfg221@gmail.com>
2025-09-23 02:33:24 -0700

    Add files via upload

---
+++ lab1/converter.js
@@ -0,0 +1,6 @@
+let fahrenheit = 36; // February average temperature in Xi'an, China (≈ 2.2°C, 2023 data)
+let celsius = (fahrenheit - 32) * 5 / 9;
+let kelvin = celsius + 273.15;
+console.log("Temperature (fahrenheit): " + fahrenheit);
+console.log("Temperature (celsius): " + celsius);
+console.log("Temperature (kelvin): " + kelvin);
